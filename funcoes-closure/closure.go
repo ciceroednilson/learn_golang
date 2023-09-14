ab4ad61edd5e882f70fb8710ae11d011306e1cc3
ciceroednilson <ciceroednilson@gmail.com>
2023-09-14 20:14:21 -0300

    closure function
    closure function

---
+++ funcoes-closure/closure.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func closure() func() {
+	texto := "Dentro da função closure"
+	funcao := func() {
+		fmt.Println(texto)
+	}
+	return funcao
+}
+
+func main() {
+	texto := "Dentro da funcao main"
+	fmt.Println(texto)
+
+	funcaoClosure := closure()
+	funcaoClosure()
+}
